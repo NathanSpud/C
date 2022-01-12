e3425fc66a3a6cd103d0080114ad890214d42d5a
Nathan Spud <Nathansv99@outlook.com>
2022-01-12 13:49:38 -0700

    Testing commits out.

--- SAIT/ITSC202/A1/prob1.c
+++ SAIT/ITSC202/A1/prob1.c
@@ -9,11 +9,42 @@ Date: 11/01/2022
 
 int main()
 {
+    float number00, average00, total00;
+    int number;
+    int i = 0;
     
+    printf("Enter 5 whole numbers.\n");
+    do
+    {
+        printf("Enter a number: ");
+        scanf("%f", &number00);
+        total00 += number00;
+        ++i;
+
+    } while (i<5);
 
 
-    
+    average00 = total00/i;
+    printf("Your total is %.0f.\nYour average is %.2f.\n\n", total00, average00);
 
+
+
+
+
+    i = 0;
+    printf("Enter any 5 numbers.\n");
+    do
+    {
+        printf("Enter a number: ");
+        scanf("%d", &number);
+        total00 += number;
+        ++i;
+
+    } while (i<5);
+
+
+    average00 = total00/i;
+    printf("Your total is %.2f.\nYour average is %.2f.", total00, average00);
 
     return 0;
 }
