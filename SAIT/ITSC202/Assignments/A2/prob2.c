198407ccdef2f50c7ec53bf79d7866cb18e41955
Nathan Spud <Nathansv99@outlook.com>
2022-01-21 21:28:01 -0700

    commit

---
+++ SAIT/ITSC202/Assignments/A2/prob2.c
@@ -0,0 +1,29 @@
+/*
+Author: NathanS
+Purpose: Print a pattern
+Date: 21/01/2022
+*/
+
+#include <stdio.h>
+
+int main(void)
+{   
+
+    int counter, row;
+
+    for( row = 1; row < 8; ++row )
+    {
+        printf("\t");
+        if ( row%2 == 1)
+        {
+            printf("*o*o*o*o*o*\n");
+        }
+        else
+        {
+            printf("o********o\n");
+        }
+    }
+
+
+    return 0;
+}
