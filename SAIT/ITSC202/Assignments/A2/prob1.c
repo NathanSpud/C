43ac89331b09782f0cb010b64b11ec713ed5225c
Nathan Spud <Nathansv99@outlook.com>
2022-02-07 10:51:21 -0700

    commit

--- SAIT/ITSC202/Assignments/A2/prob1.c
+++ SAIT/ITSC202/Assignments/A2/prob1.c
@@ -1,82 +1,26 @@
-/*
-Author: NathanS
-Purpose: Print a pattern that resembles the problems
-Date: 19/01/2021
-*/
-
 #include <stdio.h>
 
 int main(void)
 {
 
-    //declaring local variables
-    int row, asterisk;
 
+    int row, column;
 
-
-    //start printing a box of 10 width, 1st row of 10
-    printf("\t* * * * * * * * * *\n");
-    
-
-    //setting a loop to make 8 rows worth of asterisk 
-    for( row = 1; row < 9; ++row)
+    for (row = 1; row <= 10; ++row)
     {
-        //resets asterisk count every iteration, and tabs the rows
-        asterisk = 0;
         printf("\t");
-
-        //while loop that prints how many asterisks are in each row
-        //simple equation, row value = asterisk count
-        while( asterisk < row)
+        for(column = 1; column <= 10; ++column)
         {
-            printf("* ");
-            asterisk += 1;
-            
+            if (column == 1 || column == 10 || row == 1 || row ==10 || column < row)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
-        
-
-        //set of if statements to establish spaces and last star
-        if (row == 1)
-            printf("                *\n");
-
-        if (row == 2)
-            printf("              *\n");
-         
-        if (row == 3)
-            printf("            *\n");
-       
-        if (row == 4)
-            printf("          *\n");
-            
-        if (row == 5)
-            printf("        *\n");
-           
-        if (row == 6)
-            printf("      *\n");
-          
-       if (row == 7)
-            printf("    *\n");
-        
-        if (row == 8)
-            printf("  *");
-            
-        if (row == 9)
-            printf("*");
-        
-            
-            
-
-
-    
-
-
-
-
+        printf("\n");
     }
-
-    //last row of 10 asterisk
-    printf("\n\t* * * * * * * * * *");
     return 0;
 }
-
-
